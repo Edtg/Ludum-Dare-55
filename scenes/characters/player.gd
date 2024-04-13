e26b7f24bcedd0ab2ea23e528df144bf77049636
Edward Green <ed@teamgreen.co.uk>
2024-04-13 13:21:41 +0100

    Added sprinting

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 
 const MOVE_SPEED: float = 4.0
+const SPRINT_SPEED: float = 6.0
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
@@ -14,9 +15,13 @@ func _physics_process(delta):
 	var input_direction = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	
 	var direction = (transform.basis * Vector3(input_direction.x, 0, input_direction.y)).normalized()
+	var speed = MOVE_SPEED
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	
 	if direction:
-		velocity.x = direction.x * MOVE_SPEED
-		velocity.z = direction.z * MOVE_SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, 0.5)
 		velocity.z = move_toward(velocity.z, 0, 0.5)
