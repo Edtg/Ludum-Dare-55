644d08ac3d574637d0daea8268cc5d9b80460dda
Edward Green <ed@teamgreen.co.uk>
2024-04-13 18:03:21 +0100

    Added penguin spirit model

--- scenes/objects/nest.gd
+++ scenes/objects/nest.gd
@@ -3,7 +3,19 @@ extends Node3D
 
 var rock_count: int
 
+@export var required_rocks: int = 5
+
+@onready var penguin_spirit = $PenguinSpirit
+
+
 func _on_proximity_detector_body_entered(body):
 	if body.is_in_group("penguins"):
-		rock_count = body.rock_count
+		rock_count += body.rock_count
 		body.deposit_rocks()
+		check_rock_count()
+
+
+func check_rock_count():
+	if rock_count >= required_rocks:
+		rock_count -= required_rocks
+		penguin_spirit.show()
