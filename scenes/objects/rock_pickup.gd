dabfffb4724aa5fae1ec1fb0da2c59aa9ab70d40
Edward Green <ed@teamgreen.co.uk>
2024-04-14 14:35:21 +0100

    Added rock model

--- scenes/objects/rock_pickup.gd
+++ scenes/objects/rock_pickup.gd
@@ -1,5 +1,15 @@
 extends Node3D
 
+@export var random_scale_min: float = 0.8
+@export var random_scale_max: float = 1.4
+
+@onready var rock_mesh = $RockMesh
+
+
+func _ready():
+	var rock_scale = randf_range(random_scale_min, random_scale_max)
+	rock_mesh.scale = Vector3(rock_scale, rock_scale, rock_scale)
+
 
 func _on_proiximity_detector_body_entered(body):
 	if body.is_in_group("penguins"):
